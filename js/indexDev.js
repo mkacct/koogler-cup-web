af7b593e46dc242222412345ced49c5dbb4bd59e
Sam <savaka2@users.noreply.github.com>
2019-12-11 21:07:00 -0500

    add dev menu
    currently for the purpose of telling what is wrong with the pwa, when that becomes relevant

---
+++ js/indexDev.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var devClickCount = 0;
+
+$(document).ready(function() {
+	$('#devStandalone').text(mqStandalone.matches);
+	$('#devUA').text(navigator.userAgent);
+	
+	$('body').on('click', function(e) {
+		if ($(e.target).is($('#cred'))) {
+			devClickCount++;
+			if (devClickCount >= 5) {
+				devClickCount = 0;
+				if (window.confirm('Open developer menu?')) {openModal('#devModal');}
+			}
+		} else {
+			devClickCount = 0;
+		}
+	});
+});
