3ea6a6411044abc6dd19b87b03f8167c85a55340
Sam <savaka2@users.noreply.github.com>
2019-12-22 12:42:38 -0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const spreadsheetId = '12UgKkow7LXLHgf5v45EsjuPuf_7COerJ7mnLgWdNCbs'; // real da
 //const spreadsheetId = '1i5f7R7_iv4w1zMJkfoi1mLL6cFmvmhq2Cw2aPX8llUQ'; // fake data
 
 const updateTime = 60000; // to update automatically
-const reqTimeout = 30000; // set to 0 for no timeout
+const reqTimeout = 10000; // set to 0 for no timeout
 const animTime = 500; // for content loading & toasts
 const uiAnimTime = 250; // for clicking on stuff
 
