69fb05dfac6415e28b16e3c620d40bd663f24b69
Sam <savaka2@users.noreply.github.com>
2019-12-22 12:46:15 -0500

    Update config.js

--- config.js
+++ config.js
@@ -12,4 +12,5 @@ const animTime = 500; // for content loading & toasts
 const uiAnimTime = 250; // for clicking on stuff
 
 // update notes, oldest first
+// example: {version: '1.0', text: 'Added something'}
 const updateNotes = [];
