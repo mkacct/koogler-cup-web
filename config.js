17f6a99c8657858ec7a719e0928bde36ab86d7b4
Sam <savaka2@users.noreply.github.com>
2020-01-09 18:22:42 -0500

    increment patch version

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const version = '1.0.3';
+const version = '1.0.4';
 
 const apiKey = 'AIzaSyCljp-6sxk9qo3mjrvzKgVZmu-HzYtgM1c'; // google api key
 const spreadsheetId = '12UgKkow7LXLHgf5v45EsjuPuf_7COerJ7mnLgWdNCbs';
